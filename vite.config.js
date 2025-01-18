78929d44b748b24e1af95827031b69da3322162f
ollie <behoya11@gmail.com>
2025-01-18 17:37:16 +0800

    chore: add gh-pages dependency

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/supabase-todolist/",
   plugins: [react()],
   resolve: {
     alias: {
